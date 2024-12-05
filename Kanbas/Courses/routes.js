d3d811ebbc3b7fc7f9994bb320962065b1b56633
yenlnldang@gmail.com <yenlnldang@gmail.com>
2024-12-05 14:44:57 -0500

    A6- up to 5.2

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -7,18 +7,18 @@ export default function CourseRoutes(app) {
     const course = await dao.createCourse(req.body);
     res.json(course);
   });
-  app.post("/api/courses/:courseId/modules", (req, res) => {
+  app.post("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
     const module = {
       ...req.body,
       course: courseId,
     };
-    const newModule = modulesDao.createModule(module);
+    const newModule = await modulesDao.createModule(module);
     res.send(newModule);
   });
-  app.get("/api/courses/:courseId/modules", (req, res) => {
+  app.get("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
-    const modules = modulesDao.findModulesForCourse(courseId);
+    const modules = await modulesDao.findModulesForCourse(courseId);
     res.json(modules);
   });
   app.put("/api/courses/:courseId", async (req, res) => {
