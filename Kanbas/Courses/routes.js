06cfcc441e300389ad21154ec63d30e5bfb7a23e
yenlnldang@gmail.com <yenlnldang@gmail.com>
2024-12-06 11:59:23 -0500

    A6 - up to 5.3.4

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,10 +1,22 @@
 import * as dao from "./dao.js";
 import * as courseDao from "../Courses/dao.js";
 import * as modulesDao from "../Modules/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function CourseRoutes(app) {
+  const findUsersForCourse = async (req, res) => {
+    const { cid } = req.params;
+    const users = await enrollmentsDao.findUsersForCourse(cid);
+    res.json(users);
+  };
+  app.get("/api/courses/:cid/users", findUsersForCourse);
+
   app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
+    const currentUser = req.session["currentUser"];
+    if (currentUser) {
+      await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    }
     res.json(course);
   });
   app.post("/api/courses/:courseId/modules", async (req, res) => {
--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -24,6 +24,27 @@ export default function UserRoutes(app) {
   };
   app.get("/api/users/:uid/courses", findCoursesForUser);
 
+  const enrollUserInCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
+    }
+    const status = await enrollmentsDao.enrollUserInCourse(uid, cid);
+    res.send(status);
+  };
+  const unenrollUserFromCourse = async (req, res) => {
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
+    }
+    const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
+    res.send(status);
+  };
+  app.post("/api/users/:uid/courses/:cid", enrollUserInCourse);
+  app.delete("/api/users/:uid/courses/:cid", unenrollUserFromCourse);
+
   const findAllUsers = async (req, res) => {
     const { role, name } = req.query;
     if (role) {
